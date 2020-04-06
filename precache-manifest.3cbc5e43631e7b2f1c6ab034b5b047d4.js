e5e6c1220f8cf4c3c778dff1dc1e344199097a0d
Miguel Angel <miguelapb35@gmail.com>
2020-04-05 22:30:53 -0500

    Updates

---
+++ precache-manifest.3cbc5e43631e7b2f1c6ab034b5b047d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aab5e45502f392d05b815ba81ae86f24",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "5d7655172ec2026f03a3",
+    "url": "/quiz/static/css/main.e67440ce.chunk.css"
+  },
+  {
+    "revision": "4f87346707281444a441",
+    "url": "/quiz/static/js/2.b69a5514.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz/static/js/2.b69a5514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d7655172ec2026f03a3",
+    "url": "/quiz/static/js/main.147b989d.chunk.js"
+  },
+  {
+    "revision": "f53767331553a03dd250",
+    "url": "/quiz/static/js/runtime-main.d6afede4.js"
+  }
+]);
